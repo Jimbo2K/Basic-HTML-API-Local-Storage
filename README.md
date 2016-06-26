9fe8259f5887b571212d66b1a7854cd83e14f02b
Pablo Garrido Roa <pablo.garrido.roa@gamil.com>
2016-06-26 18:17:55 +0200

    README and jstorage.js 1st version added. PGR

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Basic-HTML-API-Local-Storage
-a few js functions for working width HTML API Local Storage
+# Basic HTML5 API Local Storage / *HTML5 API Local Storage básico*
+## A few js functions for working width HTML API Local Storage / *Algunas funciones para trabajar con la API de HTML5 Local Storage*
+
+**First of all:** This is my first repository, so don't expect advanced code. It is aimed at novice like me, who start programming with JavaScript.
+
+The code allows you to store values and content from html objects into the browser's local/session storage. And of course, retrieve them to set them again into the same or different objects. All done in a simply way, just using the html object or its id as a function argument.
+
+The code contains comments but in Spanish . I hope to soon add English ones, as the main purpose of the repository is to be instructive.
+***
+**Lo primero:** Este es mi primer repositorio así que no esperéis encontrar código avanzado. Está orientado a novatos como yo que empiezan a programas con JavaScript.
+
+El código permite almacenar valores y contenido de objetos HTML en el alamcenamiento local o de sesión del navegador. Y por supuesto, recuperarlos de nuevo para amacenarlos en los mismos objetos u otros distintos. Todo ello de una forma sencilla, simplemente utilizando el propio objeto o su Id como argumento de entrada de una función.
+
+El código está comentado de manera que cumpla su fin principal que es ser instructivo.
---
+++ jstorage.js
@@ -0,0 +1,171 @@
+/*Author: Pablo Garrido
+Date: 26/06/2016
+Developed with Sublime Text 3*/
+
+/********** API HTM Storage **********/
+
+//Esta función comprueba si el argumento param es un objeto (elemento del DOM),
+//un id de un elemento del DOM, o ninguno de los anteriores.
+//En los dos primeros casos devuelve el propio objeto y en el tercero null
+function selectObjId (param){
+	var out;
+	/*SI el argumento es un objeto*/
+	if (typeof(param)=== 'object') {
+		out=param;
+	/*SI el argumento es un string*/
+	} else if (typeof(param)=== 'string') {
+		/*SI el string corresponde a un Id válido*/
+		if (document.getElementById(param)) {
+			out=document.getElementById(param);
+		}
+		/*SI el string NO corresponde a un Id válido*/
+		else {
+			out=null;
+		}
+	/*SI el argumento NO es ni objeto ni string*/
+	} else {
+		out = null;
+	}
+	return out;
+}
+
+/***** SESSION STORAGE *****/
+
+//ARGUMENTO OBLIGATORIO: pobj
+function valueToSStorage(pobj){ //pobj debe ser un elemento o id de elemento válido
+	/*SI el navegador dispone del objeto Storage*/
+	if (typeof(Storage)!== 'undefined'){
+		var aux=selectObjId(pobj); //comprueba si pobj es válido para operar
+		/*SI pobj es válido Y pobj.value SI existe*/
+		if ((aux!==null) && (aux.hasOwnProperty('value'))){
+			sessionStorage.setItem(aux.id, aux.value); //value se almacena en sessionStorage con el mismo id que pobj
+		}
+		/*SI pobj NO es válido O pobj.value NO existe*/
+		else {
+			console.log('ERROR: Objeto o propiedad value inexistente'); //ERROR para el desarrollador
+		}
+	/*SI el navegador NO dispone del objeto Storage*/
+	}	else {
+		alert('Su navegador no soporta HTML5 API Storage'); //ERROR para el usuario
+		console.log('ERROR: Su navegador no soporta HTML5 API Storage'); //ERROR para el desarrollador
+	}
+
+}
+
+//ARGUMENTO OBLIGATORIO: pobj
+function inhtmlToSStorage(pobj){ //pobj debe ser un elemento o id de elemento válido
+	if (typeof(Storage)!== 'undefined'){
+		var aux=selectObjId(pobj);
+		if ((aux!==null) && (aux.hasOwnProperty('innerHTML'))){
+			sessionStorage.setItem(aux.id, aux.innerHTML);
+		}
+		else {
+			console.log('ERROR: Objeto o propiedad innerHTML inexistente');
+		}
+	}	else {
+		alert('Su navegador no soporta HTML5 API Storage');
+		console.log('ERROR: Su navegador no soporta HTML5 API Storage');
+	}
+}
+
+//ARGUMENTO OBLIGATORIO: pobj OPCIONAL: pid (si no se utiliza se entiende que pid===pobj.id)
+function sStorageToValue(pobj, pid){ //pobj debe ser un elemento o id de elemento válido
+	/*SI el navegador dispone del objeto Storage*/
+	if (typeof(Storage)!== 'undefined'){
+		var aux=selectObjId(pobj); //comprueba si pobj es válido para operar
+		var idaux=pid;
+		/*SI pobj es válido Y pobj.value SI existe*/
+		if ((aux!==null) && (aux.hasOwnProperty('value'))){
+			if (idaux=== undefined) {idaux=aux.id;} //Si no se pasa el argumento pid se asigna por defecto pobj.id
+			aux.value=sessionStorage.getItem(idaux); //El contenido de sessionStorage.idaux se alamcena en pobj.value
+		}
+		/*SI pobj NO es válido O pobj.value NO existe*/
+		else {
+			console.log('ERROR: Objeto o propiedad value inexistente'); //ERROR para el desarrollador
+		}
+	/*SI el navegador NO dispone del objeto Storage*/
+	}	else {
+		alert('Su navegador no soporta HTML5 API Storage');
+		console.log('ERROR: Su navegador no soporta HTML5 API Storage');
+	}
+}
+
+//ARGUMENTO OBLIGATORIO: pobj OPCIONAL: pid
+function sStorageToInhtml(pobj, pid){ //pobj debe ser un elemento o id de elemento válido
+	if (typeof(Storage)!== 'undefined'){
+		var aux=selectObjId(pobj);
+		var idaux=pid;
+		if ((aux!==null) && (aux.hasOwnProperty('innerHTML'))){
+			if (idaux=== undefined) {idaux=aux.id;}
+			aux.innerHTML=sessionStorage.getItem(idaux);
+		}
+		else {
+			console.log('ERROR: Objeto o propiedad value inexistente');
+		}
+	}	else {
+		alert('Su navegador no soporta HTML5 API Storage'); //ERROR para el usuario
+		console.log('ERROR: Su navegador no soporta HTML5 API Storage'); //ERROR para el desarrollador
+	}
+}
+
+/***** LOCAL STORAGE *****/
+
+//ARGUMENTO OBLIGATORIO: pobj
+function valueToLStorage(pobj){ //pobj debe ser un elemento o id de elemento válido
+	if (typeof(Storage)!== 'undefined'){
+		var aux=selectObjId(pobj);
+		if ((aux!==null) && (aux.hasOwnProperty('value'))){
+			localStorage.setItem(aux.id, aux.value);
+		}
+		else {
+			console.log('ERROR: Objeto o propiedad value inexistente');
+		}
+	}	else {
+		alert('Su navegador no soporta HTML5 API Storage');}
+}
+
+//ARGUMENTO OBLIGATORIO: pobj
+function inhtmlToLStorage(pobj){ //pobj debe ser un elemento o id de elemento válido
+	if (typeof(Storage)!== 'undefined'){
+		var aux=selectObjId(pobj);
+		if ((aux!==null) && (aux.hasOwnProperty('innerHTML'))){
+			localStorage.setItem(aux.id, aux.innerHTML);
+		}
+		else {
+			console.log('ERROR: Objeto o propiedad innerHTML inexistente');
+		}
+	}	else {
+		alert('Su navegador no soporta HTML5 API Storage');}
+}
+
+//ARGUMENTO OBLIGATORIO: pobj OPCIONAL: pid
+function lStorageToValue(pobj, pid){ //pobj debe ser un elemento o id de elemento válido
+	if (typeof(Storage)!== 'undefined'){
+		var aux=selectObjId(pobj);
+		var idaux=pid;
+		if ((aux!==null) && (aux.hasOwnProperty('value'))){
+			if (idaux=== undefined) {idaux=aux.id;}
+			aux.value=localStorage.getItem(idaux);
+		}
+		else {
+			console.log('ERROR: Objeto o propiedad value inexistente');
+		}
+	}	else {
+		alert('Su navegador no soporta HTML5 API Storage');}
+}
+
+//ARGUMENTO OBLIGATORIO: pobj OPCIONAL: pid
+function lStorageToInhtml(pobj, pid){ //pobj debe ser un elemento o id de elemento válido
+	if (typeof(Storage)!== 'undefined'){
+		var aux=selectObjId(pobj);
+		var idaux=pid;
+		if ((aux!==null) && (aux.hasOwnProperty('innerHTML'))){
+			if (idaux=== undefined) {idaux=aux.id;}
+			aux.innerHTML=localStorage.getItem(idaux);
+		}
+		else {
+			console.log('ERROR: Objeto o propiedad value inexistente');
+		}
+	}	else {
+		alert('Su navegador no soporta HTML5 API Storage');}
+}
